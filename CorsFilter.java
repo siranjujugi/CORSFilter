707b10b9092c121c3c2aae3e5c932de362bcc61d
Siranjeevi Raman <ramansiranjeevi@gmail.com>
2021-01-07 18:09:53 -0500

    Create CorsFilter.java

---
+++ CorsFilter.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package why.not.packed;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.core.Ordered;
+
+
+/**
+ * @author
+ *
+ */
+//Add the urlContext of the API used if not root
+//@Order(Ordered.LOWEST_PRECEDENCE -2)
+//@Component
+public class CorsFilter implements Filter {
+ 
+	private static Logger log = LoggerFactory.getLogger("waste-lines");
+	
+    /**
+     * Default constructor.
+     */
+    public CorsFilter() {
+    }
+ 
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+    }
+ 
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+ 
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+       log.info("CORSFilter HTTP Request: " + request.getMethod());
+ 
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        // For HTTP OPTIONS ->  ACCEPTED status code -- per CORS handshake
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+        // pass the request along the filter chain
+        chain.doFilter(request, servletResponse);
+    }
+ 
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        log.info("Initialization of CORS filter");
+    }
+	
+}
